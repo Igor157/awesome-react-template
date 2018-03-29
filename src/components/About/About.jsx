61e26350cee71f1c254c7d3ad35fabf8a1d2915d
Unknown <igor.kazakov157@gmail.com>
2018-03-29 13:50:07 +0300

    webpack config added. node modules instaled

---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class About extends React.Component {
+    render() {
+        return (
+            <div className='tmp-about tmp-page__about' >
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget tellus faucibus, scelerisque lorem nec, interdum nunc. In dignissim vehicula risus, quis porta lorem gravida vel.
+                   Proin vehicula quam ex, sit amet ultricies magna sodales a.
+                   Aenean varius tincidunt eros. Integer auctor neque sed elit mollis ultricies.
+                   Mauris aliquam hendrerit dui a consequat.
+                   Nam posuere elit nec porta mattis.
+                   Nulla auctor mattis neque vitae vehicula.
+                   Vivamus massa urna, luctus tempor libero ut, pulvinar imperdiet purus.
+                   Ut cursus velit a dolor imperdiet tempor. Quisque turpis tortor, dictum in elit sed, bibendum venenatis lacus.
+                </p>
+            </div>
+        );
+    }
+}
