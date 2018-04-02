24659cb09d2972dd286bd882eb2f07a5a0974b55
Igor Kazakov <igor.kazakov157@gmail.com>
2018-04-02 18:45:15 +0300

    redux-catch added

--- src/components/auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'igor-kazakov.eu.auth0.com',
-  clientId: 'ncQ6F8wwZIPMXhO4Ymi0ZbkGDyASS40E',
-  callbackUrl: 'http://localhost:8080/callback'
-};
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,10 @@ import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import persistState from 'redux-localstorage';
-import {aboutReducer} from './about/about.reducer';
-import {authReducer} from './auth/auth.reducer';
-import {headerReducer} from './header/header.reducer';
+import reduxCatch from 'redux-catch';
+import { aboutReducer } from './about/about.reducer';
+import { authReducer } from './auth/auth.reducer';
+import { headerReducer } from './header/header.reducer';
 
 export const allReducers = combineReducers({
     aboutReducer,
@@ -13,15 +14,21 @@ export const allReducers = combineReducers({
     headerReducer
 });
 
+function errorHandler(error, getState, lastAction) {
+    console.error(error);
+    console.debug('current state', getState());
+    console.debug('last action was', lastAction);
+}
+
 export default function configureStore(initialState) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(
-    allReducers,
-    initialState,
-    composeEnhancers(
-    applyMiddleware(thunk, logger),
-    persistState()
-  )
-);
-  return store;
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(
+        allReducers,
+        initialState,
+        composeEnhancers(
+            applyMiddleware(thunk, logger, reduxCatch(errorHandler)),
+            persistState()
+        )
+    );
+    return store;
 }
